3fc7b86714615a4b3e716d95cc9310c7f6caedd2
Sushmita <sushmita10062002@gmail.com>
2021-10-18 17:52:49 +0530

    portfolio

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+
+
+function openSlideMenu(){
+	document.getElementById('side-menu').style.width = '250px';
+	// document.getElementById('main').style.marginRight = '250px';
+}
+function closeSlideMenu(){
+    document.getElementById('side-menu').style.width = '0';
+	// document.getElementById('main').style.marginRight = '0';
+}
